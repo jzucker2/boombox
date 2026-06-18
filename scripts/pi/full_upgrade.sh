0413a2a8a5cea05b36c486cc8da2dad55ea6943c
Jordan Zucker <jordan.zucker@gmail.com>
2026-06-18 11:42:00 -0700

    Full upgrade script added

---
+++ scripts/pi/full_upgrade.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+NUMBER_STEPS=4
+
+DOCKER_CONFIG_PATH="docker/daemon.json"
+SYS_DOCKER_CONFIG_PATH="/etc/${DOCKER_CONFIG_PATH}"
+
+# https://itsfoss.com/apt-list-upgradable/
+echo "Full system upgrade to apt in ${NUMBER_STEPS} steps!"
+
+echo "[1/${STEPS}] update registry"
+sudo apt update
+
+echo "[2/${STEPS}] list upgrades"
+sudo apt list --upgradeable
+
+
+echo "[3/${STEPS}] remove docker config before update process at SYS_DOCKER_CONFIG_PATH: ${SYS_DOCKER_CONFIG_PATH}"
+sudo rm sudo rm $SYS_DOCKER_CONFIG_PATH
+
+
+echo "[4/${STEPS}] full upgrade apt"
+sudo apt full-upgrade -y
+
+
+echo "[5/${STEPS}] now clean up"
+sudo apt autoremove -y
+
+echo "Done updating apt stuffz in ${NUMBER_STEPS} steps!!!"
+
+#echo "Now, reboot!"
+#sudo reboot
