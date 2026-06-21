b41e4a647afc1ab670ceeef1ab103ab81ca48f26
Jordan Zucker <jordan.zucker@gmail.com>
2026-06-21 15:27:56 -0700

    Gah, fixed

--- scripts/pi/full_upgrade.sh
+++ scripts/pi/full_upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-NUMBER_STEPS=4
+NUMBER_STEPS=5
 
 DOCKER_CONFIG_PATH="docker/daemon.json"
 SYS_DOCKER_CONFIG_PATH="/etc/${DOCKER_CONFIG_PATH}"
@@ -8,22 +8,22 @@ SYS_DOCKER_CONFIG_PATH="/etc/${DOCKER_CONFIG_PATH}"
 # https://itsfoss.com/apt-list-upgradable/
 echo "Full system upgrade to apt in ${NUMBER_STEPS} steps!"
 
-echo "[1/${STEPS}] update registry"
+echo "[1/${NUMBER_STEPS}] update registry"
 sudo apt update
 
-echo "[2/${STEPS}] list upgrades"
+echo "[2/${NUMBER_STEPS}] list upgrades"
 sudo apt list --upgradeable
 
 
-echo "[3/${STEPS}] remove docker config before update process at SYS_DOCKER_CONFIG_PATH: ${SYS_DOCKER_CONFIG_PATH}"
+echo "[3/${NUMBER_STEPS}] remove docker config before update process at SYS_DOCKER_CONFIG_PATH: ${SYS_DOCKER_CONFIG_PATH}"
 sudo rm sudo rm $SYS_DOCKER_CONFIG_PATH
 
 
-echo "[4/${STEPS}] full upgrade apt"
+echo "[4/${NUMBER_STEPS}] full upgrade apt"
 sudo apt full-upgrade -y
 
 
-echo "[5/${STEPS}] now clean up"
+echo "[5/${NUMBER_STEPS}] now clean up"
 sudo apt autoremove -y
 
 echo "Done updating apt stuffz in ${NUMBER_STEPS} steps!!!"
